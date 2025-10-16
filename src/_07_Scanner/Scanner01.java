410b6401db9f7d55731c4751f4f51720455a08a9
youngwhan0420-sketch <youngwhan0420@gmail.com>
2025-10-16 14:05:03 +0900

    Feat: String

--- src/_07_Scanner/Scanner01.java
+++ src/_07_Scanner/Scanner01.java
@@ -19,11 +19,13 @@ public class Scanner01 {
 
         //nextLine()는 엔터 단위로 읽는다.
         String str3 = scanner.nextLine();
-        System.out.println("str3: " + str3);
+        System.out.println("str3 :" + str3);
         //중간에 공백이 있더라도 한줄당 한줄로 인식을 받는다.
         //입력에 숫자를 써도 문자열로 인식한다.
 
         //int 정수 입력 받기
+        //nextInt() 개행문자를 무시하고 입력받는다.
+        //엔터를 치면 엔터 전까지 입력받고 엔터의 개행문자는 남게 됨
 //        int num1 = scanner.nextInt();
 //        System.out.println("num1: " + num1);
 //        //이렇게 하면 정수를 입력 받을 수 있다.
@@ -36,7 +38,12 @@ public class Scanner01 {
         System.out.println("num: " + num + "str: " + str);
         //nextint치고 nextline받을려니까 끝나는 이유는 123누르고 엔터를 누르는 순간 123\n까지 입력을 받게되기 때문에
         //123은 넘에 들어가는데 \n기록이 남으니 nextLine으로 가니까 엔터를 받고 끝내버리기 때문에 결과라 이렇게 나온다.
-        //이걸 해결 하는 방법은 중간에 \n를 버려줘야 하니 중간에 scanner.nextLine();를 써서 버려준다.
+        //이걸 해결 하는 방법은 중간에 \n를 버려줘야 하니 중간에 scanner.nextLine();를 써서 버려준다
+
+
+
+
+
 
     }
 }
